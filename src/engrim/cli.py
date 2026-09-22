ccfda86711dfda62577367c9db0a037cb8479a54
Tim Gordon <timgordontg@gmail.com>
2026-09-21 22:53:48 -0400

    chore: bump version to 1.4.10

--- src/engrim/cli.py
+++ src/engrim/cli.py
@@ -40,7 +40,7 @@ import sys
 try:
     from engrim import __version__
 except ImportError:
-    __version__ = "1.4.9"
+    __version__ = "1.4.10"
 
 DEFAULT_DB = os.path.expanduser("~/.engrim/memory.db")
 TYPES = ("decision", "fact", "feedback", "state", "reference", "user")
